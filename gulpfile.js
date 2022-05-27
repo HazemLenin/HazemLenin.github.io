70136696e50bb09127d3c5bc31bb3eccf93d3847
HazemLenin <63303495+HazemLenin@users.noreply.github.com>
2022-05-27 22:48:41 +0200

    second release

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+
+function minifyImages() {
+    return gulp.src('./images/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./minified/images'))
+}
+
+gulp.task("minifyImages", minifyImages);
+
+gulp.task("watch", () => {
+    gulp.watch("./images/*", minifyImages)
+});
+
+gulp.task("default", gulp.series("minifyImages", "watch")) 
